335e6c8fe0625daa6ec371b8ee233f2beb45de6b
Arjun Patel <145498145+ArjunPatel32@users.noreply.github.com>
2024-07-15 19:23:09 +0000

    adjustments

--- main/customization.py
+++ main/customization.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import base64
 
+
 def set_background(image_path: str):
     """function to set background image of website"""
     # function found from here: https://discuss.streamlit.io/t/how-do-i-use-a-background-image-on-streamlit/5067/15
@@ -21,6 +22,7 @@ def set_background(image_path: str):
         unsafe_allow_html=True,
     )
 
+
 def create_footer():
     st.markdown("""
     <hr style='border: none; border-top: 1px dashed gray;'>
@@ -29,14 +31,21 @@ def create_footer():
     </footer>
     """, unsafe_allow_html=True)
 
+
 def main_content():
+    st.header("_School & Activities_", divider='blue')
     st.markdown(
         """
         <h4>I'm currently a sophomore at UC Berkeley studying Astrophysics and Applied Math, with a concentration in statistics.</h4>
         <h4>I'm passionate about astronomy, coding, poker, video games, and lifting. I'm always looking for more ways to explore my interests, especially poker. If you know of any good resources or want to play a game, feel free to reach out!</h4>
-        <h4>I am an Assistant Telescope Officer at the Undergraduate Astronomy Society at UC Berkeley, operating various telescopes for events. I've been trained in activating and functioning different telescopes, including Treffers, Cassegrain, Newtonian, & EV.</h4>
+        <h4>I am an Assistant Telescope Officer at the Undergraduate Astronomy Society at UC Berkeley, operating various telescopes for events. I've been trained in activating and functioning different telescopes, including Treffers, Cassegrain, Newtonian, & EV.</h4>""",
+        unsafe_allow_html=True
+    )
+    st.header("_Work_", divider='blue')
+    st.markdown(
+        """
         <h4>I'm currently working as a Data Science Intern at Dotlas. I've developed web scrapers and data collectors at Dotlas, gathering and analyzing large datasets. I also worked with different LLMs and APIs to automate data collection and analysis.</h4>
-        <h4>I've also worked as a Research Assistant at the University of Rochester Laboratory for Laser Energetics, where I utilized COMSOL Multiphysics for modeling experiments on electromagnetic radiation detection. I explored the use of terahertz radiation to differentiate between healthy and cancerous tissues and evaluated the impact of embedding tissues in paraffin wax to mimic real-life samples.</h4>
+        <h4>I've also worked as a Research Assistant at the University of Rochester Laboratory for Laser Energetics, where I utilized COMSOL Multiphysics® for modeling experiments on electromagnetic radiation detection. I explored the use of terahertz radiation to differentiate between healthy and cancerous tissues and evaluated the impact of embedding tissues in paraffin wax to mimic real-life samples.</h4>
         <h4>Check out my socials and feel free to reach out if you want to chat or collaborate!</h4>
         """,
         unsafe_allow_html=True

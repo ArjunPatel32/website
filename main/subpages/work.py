b78d50c236366f7af46d03d0ffc21bad3d37a244
Arjun Patel <arjun_patel3@berkeley.edu>
2025-01-14 12:16:14 -0500

    description change 1-14-25

--- main/subpages/work.py
+++ main/subpages/work.py
@@ -19,27 +19,28 @@ def display_work():
     col1, col2 = st.columns([3, 1])
 
     with col1:
-        # Display the Rubin Observatory image on the left side
-        st.image('main/images/rubin_observatory.jpg', width=250)
-
-    with col2:
         # Rubin Observatory work details with increased font size
         st.markdown(
             """
             <p style="font-size: 28px;">
-            I am starting research under the mentorship of Dean Steven Kahn at the Rubin Observatory, focusing on data analysis and observational techniques 
-            that will contribute to the ongoing commissioning and science goals of the observatory.
+            I am currently conducting research under the mentorship of Dean Steven Kahn at the Rubin Observatory. 
+            I've engineered a pipeline to analyze and compare telescope chamber temperatures across different date ranges, and 
+            automated 2D and 3D visualizations to illustrate variations in elevation and temperature across axis offsets.
             </p>
             """, 
             unsafe_allow_html=True
         )
+
+    with col2:
+        # Display the Rubin Observatory image on the left side
+        st.image('main/images/rubin_observatory.jpg', width=250)
 
     # Second section: NASA Project
     col3, col4 = st.columns([1, 3])
 
     with col3:
             # Display the NASA logo on the right side
-            st.image('main/images/rubin_observatory.jpg', width=200)
+            st.image('main/images/nasa_logo.jpg', width=200)
 
     with col4:
         # NASA project work experience details with increased font size
@@ -63,7 +64,7 @@ def display_work():
             """
             <p style="font-size: 28px;">
             I've also worked as a Data Science Intern at <a href="https://www.dotlas.com/" target="_blank">Dotlas</a>. 
-            I've developed web scrapers and data collectors, gathering and analyzing large datasets. 
+            where I've developed web scrapers and data collectors, gathering and analyzing large datasets. 
             I also worked with different LLMs and APIs to automate data collection and analysis, as well as built a 
             <a href="https://catalog.dotlas.com" target="_blank">data-display website</a> for clients.
             </p>
